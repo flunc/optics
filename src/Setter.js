2f6dd06fb2625b27673a602eaf54c3b79c3036d9
Scott Christopher <scott.christopher@finder.com>
2016-05-02 10:47:15 +1000

    Add Setter.setJust

--- src/Setter.js
+++ src/Setter.js
@@ -1,5 +1,8 @@
 const
-  R = require('ramda');
+  R  = require('ramda'),
+  RF = require('ramda-fantasy'),
+  
+  Just = RF.Maybe.Just;
 
 // over :: Setter s t a b -> (a -> b) -> s -> t
 const over = R.curry((l, f, s) => l(f)(s));
@@ -7,11 +10,14 @@ const over = R.curry((l, f, s) => l(f)(s));
 // set :: Setter s t a b -> b -> s -> t
 const set = R.curry((l, b, s) => over(l, _ => b, s));
 
+const setJust = R.curry((l, b, s) => set(l, Just(b), s));
+
 // mapped :: Functor f => Setter (f a) (f b) a b
 const mapped = R.map;
 
 module.exports = {
   over,
   set: set, // `set` doesn't play nicely with object shorthand syntax
+  setJust,
   mapped
 };
